6d28060a8ab0412af47bd7f16a4ec4fc09fc50f3
monkey-master <admin@rhel.cc>
2017-11-03 18:02:39 +0800

    add

--- forms/command.go
+++ forms/command.go
@@ -2,7 +2,8 @@ package forms
 
 type CommandForm struct {
 	Path    string `form:"path" json:"path"`
-	Command string `form:"command" json:"command" binding:"required"`
+	Command string `form:"command" json:"command"`
 	Html    string `form:"html" json:"html" `
 	Token   string `form:"token" json:"token" binding:"required"`
+	CommandID string `form:"commandID" json:"commandID" `
 }
